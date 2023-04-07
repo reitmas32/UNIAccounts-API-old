14428bd14a4d9568865f0127e0325c6986d1be59
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-06 21:17:00 -0600

    feat: Add check_service_permissions

--- src/config.py
+++ src/config.py
@@ -1,1 +1,25 @@
 API_VERSION = 'v1'
+
+SERVICE_PERMISSIONS = {
+    'UNICA_STORE': ['singup_route_POST']
+}
+
+import ENVS
+
+def valid_API_KEY(API_KEY: str) :
+    for key in ENVS.API_KEYS:
+        value = ENVS.API_KEYS[key]
+        if value == API_KEY:
+            return True, key
+    return False, ''
+
+def check_service_permissions(service_name: str, permission_route: str):
+    try:
+        permissions = SERVICE_PERMISSIONS[service_name]
+        for permission in permissions:
+            if permission == permission_route:
+                return True
+    except SystemError as e:
+        print(e)
+        
+    return False
