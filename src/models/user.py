823b3616c53cdd48b90a13d170d9967709b9b72a
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-08 05:34:38 -0600

    feat: Add status_code to response of func

--- src/models/user.py
+++ src/models/user.py
@@ -30,13 +30,13 @@ class User:
     def from_dict(obj: dict):
         
         if obj.get('account_number') == None:
-            return None, 'No account_number send in the request'
+            return None, 'No account_number send in the request', 428
         if obj.get('password') == None:
-            return None, 'No password send in the request'
+            return None, 'No password send in the request', 428
         if obj.get('role') == None or obj.get('role_key') == None:
-            return None, 'No role send in the request'
+            return None, 'No role send in the request', 428
         if obj.get('account_number') == None:
-            return None, 'No email, nick_name'
+            return None, 'No email, nick_name', 428
         # Auth
         nick_name           = str(obj.get("nick_name"))
         password            = generate_password_hash(str(obj.get("password")))
@@ -52,14 +52,14 @@ class User:
         # Data Academic
         account_number      = str(obj.get("account_number"))
         if not account_number.isdigit() and len(account_number) != 9:
-            return None, 'account_number invalid'
+            return None, 'account_number invalid', 428
         careers             = str(obj.get("careers"))
         half_year           = int(obj.get("half_year"))
         
         # Permissions
         role                = str(obj.get("role"))
         role_key            = str(obj.get("role_key"))
-        return User(nick_name,password,email,name ,last_name_fathers ,last_name_mothers,account_number,careers,role ,role_key,half_year), 'Succesful User'
+        return User(nick_name,password,email,name ,last_name_fathers ,last_name_mothers,account_number,careers,role ,role_key,half_year), 'Succesful User', 200
     
     def __dict__(self):
         data = {
