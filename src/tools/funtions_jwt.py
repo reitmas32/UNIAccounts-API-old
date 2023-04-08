65d5d1aee4ba261f70cfbfc99f8f5a8f31003275
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-08 14:33:14 -0600

    feat: The way in which the variable is obtained was changed

--- src/tools/funtions_jwt.py
+++ src/tools/funtions_jwt.py
@@ -4,6 +4,8 @@ from os import getenv
 from datetime import datetime, timedelta
 from flask import jsonify
 
+import ENVS
+
 
 def expire_date(days: int):
     now = datetime.now()
@@ -13,15 +15,15 @@ def expire_date(days: int):
 
 def write_token(data: dict):
     token = encode(payload={**data, "exp": expire_date(2)},
-                   key=getenv("SECRET"), algorithm="HS256")
+                   key=ENVS.SECRET_KEY_TOKEN, algorithm="HS256")
     return token.encode("UTF-8")
 
 
 def validate_token(token, output=False):
     try:
         if output:
-            return decode(token, key=getenv("SECRET"), algorithms=["HS256"])
-        decode(token, key=getenv("SECRET"), algorithms=["HS256"])
+            return decode(token, key=ENVS.SECRET_KEY_TOKEN, algorithms=["HS256"])
+        decode(token, key=ENVS.SECRET_KEY_TOKEN, algorithms=["HS256"])
     except exceptions.DecodeError:
         response = jsonify({"message": "Invalid Token"})
         response.status_code = 401
