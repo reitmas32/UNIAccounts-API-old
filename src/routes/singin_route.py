57eb7d0314a23398ad6c93324a3c57685a4cc117
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-08 14:31:59 -0600

    fix: Change type request of func

--- src/routes/singin_route.py
+++ src/routes/singin_route.py
@@ -6,6 +6,6 @@ def singin_route_PUT(parameters_json: dict, data_base: IDataBase):
     new_user, message, status_code = User.from_dict_password_nohash(parameters_json)
     if new_user == None:
         return {'message': f'Error {message}', 'status_code': status_code}, status_code
-    message, status_code = data_base.find_user(new_user)
-    request = {'message': message, 'status_code': status_code}
+    request = data_base.find_user(new_user)
+    request = {'message': request.get('message'), 'status_code': request.get('status_code'), 'jwt': request.get('jwt')}
     return request, status_code
