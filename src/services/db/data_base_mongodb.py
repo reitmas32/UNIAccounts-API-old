ecccde3c08f163cde4ff383c10d0fbc7b55f3b6b
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-08 05:35:30 -0600

    feat: Create interface specialization

---
+++ src/services/db/data_base_mongodb.py
@@ -0,0 +1,17 @@
+import json
+from models.user import User
+from services.db.idata_base import IDataBase
+from flask_pymongo import PyMongo
+
+
+class DataBase_MongoDB(IDataBase):
+    _data_base = None
+    def __init__(self, app):
+        self._data_base = PyMongo(app)
+    
+    def create_user(self, user: User):
+        if self._data_base.db.users.find_one({'nick_name': user.nick_name}) != None:
+            return 'Error nick_name in use', 428
+        id = self._data_base.db.users.insert_one(user.__dict__())
+        return 'Succesfull SingUp', 200
+        
