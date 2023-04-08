9ca146da9a008d8fd2ef599be3fe180aea5f1068
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-08 06:38:30 -0600

    feat: Create singin_route_PUT endpoint

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ data_base_service = DataBase_MongoDB(app)
 
 import config as CONFIG
 from routes.singup_route import *
+from routes.singin_route import *
 
 @app.route('/', methods=['GET'])
 def index():
@@ -35,6 +36,26 @@ def singup():
         
     if request.method == 'POST':
         response, status_code = singup_route_POST(request.json, data_base_service)
+    
+    return response, status_code
+
+@app.route('/api/v1/singin', methods=['PUT'])
+def singin():
+    status, service_name = CONFIG.valid_API_KEY(request.headers.get('API_KEY'))
+    
+    response = 'Nada'
+    status_code = 404
+    
+    if not request.is_json:
+        return 'No content Type'
+    if not status:
+        return 'No API_KEY Valid', 401
+    
+    if not CONFIG.check_service_permissions(service_name, f'singin_route_{request.method}'):
+        return 'API_KEY Valid but Permission Denied by this request', 403
+        
+    if request.method == 'PUT':
+        response, status_code = singin_route_PUT(request.json, data_base_service)
     
     return response, status_code
 
