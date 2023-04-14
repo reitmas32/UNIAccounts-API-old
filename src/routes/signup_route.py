f74508b5e140337a9b1e458cc964a39986e81b02
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-14 01:56:15 -0600

    feat: Add service_name param when calling create_user

--- src/routes/signup_route.py
+++ src/routes/signup_route.py
@@ -3,10 +3,10 @@ from services.db.idata_base import IDataBase
 from models.user import User
 
 
-def signup_route_POST(parameters_json: dict, data_base: IDataBase):
+def signup_route_POST(parameters_json: dict, data_base: IDataBase, service_name: str):
     new_user = User.from_dict(parameters_json)
     if new_user == None:
         return {'message': 'Error User no created', 'status_code': 428}
-    message, status_code = data_base.create_user(new_user)
+    message, status_code = data_base.create_user(new_user, service_name)
     response = {'message': message, 'status_code': status_code}
     return response
