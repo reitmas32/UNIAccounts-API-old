e0ffd05e67b64998819106cdfa4fb8361293f985
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-14 01:57:19 -0600

    feat: Change Interface by DataBase

--- src/services/db/idata_base.py
+++ src/services/db/idata_base.py
@@ -5,9 +5,15 @@ class IDataBase:
     def __init__():
         pass
     
-    def create_user(self, user: User):
+    def create_user(self, user: User, service_name: str):
         pass
     
-    def find_user(self, user: User):
+    def find_user(self, user: User, service_name: str):
+        pass
+    
+    def signin_user(self, user: User, service_name: str):
+        pass
+    
+    def _create_new_session(self, JWT: str, service_name: str):
         pass
     
