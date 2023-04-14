78c1e26911cf5f146388567c76a0afee34de1d05
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-14 02:06:03 -0600

    feat: Add service_name param when calling find_user

--- src/routes/signin_route.py
+++ src/routes/signin_route.py
@@ -3,11 +3,11 @@ from models.user import User
 from tools.funtions_jwt import validate_token
 
 
-def signin_route_PUT(parameters_json: dict, data_base: IDataBase):
+def signin_route_PUT(parameters_json: dict, data_base: IDataBase, service_name: str):
     new_user = User.from_dict_password_nohash(parameters_json)
     if new_user == None:
         return {'message': 'Error User no created', 'status_code': 428}
-    response = data_base.find_user(new_user)
+    response = data_base.find_user(new_user, service_name)
     response = {'message': response.get('message'), 'status_code': response.get('status_code'), 'jwt': response.get('jwt')}
     return response
 
