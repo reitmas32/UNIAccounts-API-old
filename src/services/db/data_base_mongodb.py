dc0654aafcbb5ea528cb919faed2b51c0acf4f10
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-14 03:19:28 -0600

    reafactor: The responsibilities of the function were divided

--- src/services/db/data_base_mongodb.py
+++ src/services/db/data_base_mongodb.py
@@ -5,6 +5,8 @@ from flask_pymongo import PyMongo
 from werkzeug.security import check_password_hash
 import tools.functions_dict as TOOLS_Dict
 
+def check_password_match(user_request: dict, user: User):
+    return check_password_hash(user_request.get("password"), user.password)
 
 class DataBase_MongoDB(IDataBase):
     _data_base = None
@@ -36,19 +38,28 @@ class DataBase_MongoDB(IDataBase):
         if request_mongodb == None:
             return {'message':'Error no User Exist', 'status_code' :403}
         
-        if check_password_hash(request_mongodb.get("password"), user.password) == True:
+        return {'message':'Succesfull signIn', 'status_code' :200, 'request_mongodb': request_mongodb}
+    
+    def signin_user(self, user: User, service_name: str):
+        response_find_user = self.find_user(user, service_name)
+        
+        user_response = response_find_user.get("request_mongodb")
+        user_jwt = ''
+        
+        # Generate JWT
+        if check_password_match(user_response, user) == True:
             user.password = ''
             user_jwt = write_token(data=user.__dict__())
         else:
             return {'message':'Password Invalid', 'status_code' :403}
-        return {'message':'Succesfull signIn', 'status_code' :200, 'jwt': str(user_jwt, encoding='UTF-8')}
-    
-    def signin_user(self, user: User, service_name: str):
-        response_find_user = self.find_user(user, service_name)
-        if TOOLS_Dict.get_from_dict(response_find_user, 'status_code', default_value=0) != 200:
-            return response_find_user
+        
+        
+        response = {'message':'Succesfull signIn', 'status_code' :200, 'jwt': str(user_jwt, encoding='UTF-8'), 'service_name': service_name}
+        
+        if TOOLS_Dict.get_from_dict(response, 'status_code', default_value=0) != 200:
+            return response
         _ = self._create_new_session(TOOLS_Dict.get_from_dict(response_find_user, 'jwt'), service_name=service_name)
-        return response_find_user
+        return response
     
     def _create_new_session(self, JWT: str, service_name: str):
         return self._data_base.db.session.insert_one({'JWT': JWT, 'service_name': service_name, 'status': 'ACTIVE'})
