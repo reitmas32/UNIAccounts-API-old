86edba632f045b9a01809615673606086c5b7256
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-14 22:35:52 -0600

    doc: Added documentation to the functions and the package

--- src/config.py
+++ src/config.py
@@ -1,3 +1,17 @@
+######################################################################
+# author = Rafael Zamora
+# copyright = Copyright 2020-2023, UNICA-ManagerAccounts
+# date = 06/04/2023
+# license = PSF
+# version = 1.0
+# maintainer = Rafael Zamora
+# email = rafa.zamora.rals@gmail.com
+# status = Development
+######################################################################
+
+# Local Packages
+import ENVS
+
 API_VERSION = 'v1'
 
 SERVICE_PERMISSIONS = {
@@ -11,22 +25,39 @@ DEBUG = True
 
 HOST = '0.0.0.0'
 
-import ENVS
 
-def valid_API_KEY(API_KEY: str) :
+def valid_API_KEY(API_KEY: str):
+    """Validates if an API_KEY is authorized for the use of the API
+
+    Args:
+        API_KEY (str)
+
+    Returns:
+        tuple[bool, str]: Status of API_KEY, name of the service to which the API_KEY belongs
+    """
     for key in ENVS.API_KEYS:
         value = ENVS.API_KEYS[key]
         if value == API_KEY:
             return True, key
     return False, ''
 
-def check_service_permissions(service_name: str, permission_route: str):
+
+def check_service_permissions(service_name: str, end_point: str):
+    """Verifies if the service is authorized to use the requested EndPoint
+
+    Args:
+        service_name (str)
+        end_point (str)
+
+    Returns:
+        bool: [True] if valid permission, opposite case [False]
+    """
     try:
         permissions = SERVICE_PERMISSIONS[service_name]
         for permission in permissions:
-            if permission == permission_route:
+            if permission == end_point:
                 return True
     except SystemError as e:
         print(e)
-        
+
     return False
