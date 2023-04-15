dd50109ad0bf98938d41f4542f547ba3d1a4e917
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-15 04:00:04 -0600

    docs: Creation of the  documentation of funcs

--- src/tools/functions_dict.py
+++ src/tools/functions_dict.py
@@ -1,4 +1,25 @@
+######################################################################
+# author = Rafael Zamora
+# copyright = Copyright 2023, UNICA-ManagerAccounts
+# date = 10/04/2023
+# license = PSF
+# version = 1.0
+# maintainer = Rafael Zamora
+# email = rafa.zamora.rals@gmail.com
+# status = Development
+######################################################################
+
 def get_from_dict(dictionary: dict, key: str, default_value = ''):
+    """Wrapper to get a dictionary value
+
+    Args:
+        dictionary (dict): Data Dictionary
+        key (str):
+        default_value (str, optional): Default value in case there is no coincidence
+
+    Returns:
+        Any: value
+    """
     value = dictionary.get(key)
     
     if value == None:
