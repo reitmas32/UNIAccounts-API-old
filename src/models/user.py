1cae6761bf38157c97908cfec7820fd6599f2c4d
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-15 04:16:31 -0600

    refactor: remove deprecated funcs

--- src/models/user.py
+++ src/models/user.py
@@ -26,23 +26,6 @@ class User:
                 self.role  = role 
                 self.role_key = role_key
                 self.half_year = half_year
-                
-    @staticmethod
-    def from_dict_password_nohash_deprecated(obj: dict):
-        
-        if obj.get('account_number') != None or obj.get('nick_name') != None or obj.get('email') != None:
-            pass
-        else:
-            return None, 'No account_number, nick_name, or email send in the request', 428
-        if obj.get('password') == None:
-            return None, 'No password send in the request', 428
-        # Auth
-        nick_name           = str(obj.get("nick_name"))
-        password            = str(obj.get("password"))
-        account_number      = str(obj.get("account_number"))
-        email               = str(obj.get("email"))
-
-        return User(nick_name=nick_name,password=password,email=email,account_number=account_number), 'Succesful User', 200
 
     @staticmethod
     def from_dict_password_nohash(obj: dict):
@@ -75,56 +58,6 @@ class User:
         role                = str(TOOLS_Dict.get_from_dict(obj, "role"))
         role_key            = str(TOOLS_Dict.get_from_dict(obj, "role_key"))
         return User(nick_name,password,email,name ,last_name_fathers ,last_name_mothers,account_number,careers,role ,role_key,half_year)
-    
-    def __dict__(self):
-        data = {
-            'nick_name'         : self.nick_name,
-            'password'          : self.password,
-            'email'             : self.email,
-            'name'              : self.name,
-            'last_name_fathers' : self.last_name_fathers,
-            'last_name_mothers' : self.last_name_mothers,
-            'account_number'    : self.account_number,
-            'careers'           : self.careers,
-            'half_year'         : self.half_year,
-            'role'              : self.role,
-            'role_key'          : self.role_key,
-        }
-        
-        return data       
-    @staticmethod
-    def from_dict_deprecated(obj: dict):
-        
-        if obj.get('account_number') == None:
-            return None, 'No account_number send in the request', 428
-        if obj.get('password') == None:
-            return None, 'No password send in the request', 428
-        if obj.get('role') == None or obj.get('role_key') == None:
-            return None, 'No role send in the request', 428
-        if obj.get('account_number') == None:
-            return None, 'No email, nick_name', 428
-        # Auth
-        nick_name           = str(obj.get("nick_name"))
-        password            = generate_password_hash(str(obj.get("password")))
-        # Email
-        email               = str(obj.get("email"))
-        
-        # Name
-        name                = str(obj.get("name")).upper()
-        last_name_fathers   = str(obj.get("last_name_fathers")).upper()
-        last_name_mothers   = str(obj.get("last_name_mothers")).upper()
-        
-        # Data Academic
-        account_number      = str(obj.get("account_number"))
-        if not account_number.isdigit() and len(account_number) != 9:
-            return None, 'account_number invalid', 428
-        careers             = str(obj.get("careers"))
-        half_year           = int(obj.get("half_year"))
-        
-        # Permissions
-        role                = str(obj.get("role"))
-        role_key            = str(obj.get("role_key"))
-        return User(nick_name,password,email,name ,last_name_fathers ,last_name_mothers,account_number,careers,role ,role_key,half_year), 'Succesful User', 200
     
     def __dict__(self):
         data = {
