fdf7ed72e41c37a761427ef58b54502135f48cee
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-16 03:25:20 -0600

    refactor: Move location of script

--- tools/generate_docs_api.py
+++ tools/generate_docs_api.py
@@ -6,9 +6,9 @@ def generate_markdown(config: dict, this_dir: str, version_api: str):
     data = ""
     try:
         for file in config["files"]:
-            with open(f"{this_dir}/{version_api}/{file}") as file:
+            with open(f"{this_dir}/docs/{version_api}/{file}") as file:
                 data += file.read() + '\n'
-        with open (f"{this_dir}/api_{version_api}.md", 'w') as file:
+        with open (f"{this_dir}/docs/api_{version_api}.md", 'w') as file:
             file.write(data)
         print(f"Successful Generate API Docs")
     except ValueError as e:
@@ -31,7 +31,7 @@ def main():
         
         this_dir = os.getcwd()
         
-        file_config = open(f"{this_dir}/{version_api}/config.json", "r")
+        file_config = open(f"{this_dir}/docs/{version_api}/config.json", "r")
         config = json.load(file_config)
         
         output_types[f"{output_type}"](config=config, this_dir=this_dir, version_api=version_api)
