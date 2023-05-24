d3bfe529e846a6cff845a619c990eb5f7a8d6fa8
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-23 18:09:33 -0600

    feat: Create EndPoint by SignUp use

--- src/api/v1/routers/users.py
+++ src/api/v1/routers/users.py
@@ -1,5 +1,9 @@
 # External Packages
 from flask import Blueprint, request
+from flask import render_template
+import json
+import requests
+import config.base as CONFIG
 
 from tools.functions_authentication import valid_headers
 
@@ -24,3 +28,43 @@ def signup():
     if request.method == "POST":
         response = signup_route_POST(parameters_json=request.get_json())
         return response
+
+@views_users.route("/signup", methods=["GET", "POST"])
+def signup_app():
+    """SignUp EndPoint
+
+    Returns:
+        dict: JSON response
+        int: status code of the request
+    """
+    
+    if request.method == 'POST':
+        
+        name   = request.form['name']
+        last_name   = request.form['last_name']
+        email  = request.form['email']
+        user_name  = request.form['user_name']
+        phone_number = request.form['phone_number']
+        date_of_birth = request.form['date_of_birth']
+        password  = request.form['password']
+        role   = request.form['role']
+        
+        query = {
+            'name'              : name,
+            'last_name'         : last_name,
+            'email'             : email,
+            'user_name'         : user_name,
+            'phone_number'      : phone_number,
+            'date_of_birth'     : date_of_birth,
+            'password'          : password,
+            'role'              : role,
+        }
+        response = requests.post(f'http://127.0.0.1:{CONFIG.PORT}/api/v1/signup',json=query, headers={'Api-Key': CONFIG.config("API-KEY"), 'Service': CONFIG.config("SERVICE")} )
+        response_json = json.loads(response.content)
+
+        if(response_json.get('Success') == True):
+            return render_template('successful_signup.html')
+        else:
+            return render_template('error_signup.html')
+
+    return render_template('signup.html')
