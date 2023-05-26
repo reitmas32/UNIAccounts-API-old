b818a4b9bfe73fad4cace5a25958afd95468920a
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-26 12:16:34 -0600

    feat: Add Model word to Name of Class

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from models.services import ServicesModel
 from models.user_services import UserServicesModel
 from models.users import UsersModel
-from models.forgot_codes import ForgotCode
+from models.forgot_codes import ForgotCodeModel
--- src/models/forgot_codes.py
+++ src/models/forgot_codes.py
@@ -3,7 +3,7 @@ from config.database import db
 from .base_model import BaseModelClass
 
 
-class ForgotCode(BaseModelClass):
+class ForgotCodeModel(BaseModelClass):
     __tablename__ = "forgot_codes"
     user_name = db.Column(db.String(255), nullable=False)
     code = db.Column(db.String(255), nullable=False)
