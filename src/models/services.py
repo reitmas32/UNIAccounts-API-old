439cc6458cefe4c79dd9e71504879c38e6916233
reitmas32 <rafa.zamora.rals@gmail.com>
2023-07-01 23:35:07 -0600

    feat: create toJSON func

--- src/models/services.py
+++ src/models/services.py
@@ -9,3 +9,15 @@ class ServicesModel(BaseModelClass):
     mother_login = db.Column(db.Boolean, default=False)
     api_key = db.Column(db.String(255), nullable=False)
     jwt_exp = db.Column(db.Integer, nullable=False)
+
+    def toJSON(self):
+        return {
+            "name": self.name,
+            "mother_login": self.mother_login,
+            "api_key": self.api_key,
+            "jwt_exp": self.jwt_exp,
+            "id": self.id,
+            "updated": self.updated.strftime('%Y-%m-%d %H:%M:%S'),
+            "created": self.created.strftime('%Y-%m-%d %H:%M:%S'),
+            "is_removed": self.is_removed,
+        }
