833aaea3d0391cf2c30d8ac869a194c618954b94
reitmas32 <rafa.zamora.rals@gmail.com>
2023-07-08 23:06:05 -0600

    feat: remove prefix

--- src/config/api_docs.py
+++ src/config/api_docs.py
@@ -3,7 +3,7 @@ from flask_restx import Api, Resource, fields
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask import Blueprint, request
 
-api = Api(app, version='1.0', title='UNIAccount API', description='API to Manage UNICA System Accounts', license='Apache 2.0',  prefix='/api/v1')
+api = Api(app, version='1.0', title='UNIAccount API', description='API to Manage UNICA System Accounts', license='Apache 2.0')
 
 # Configura la interfaz de usuario de Swagger
 SWAGGER_URL = '/docs/api/v1'
