5a8d58b25d50f68be8c11597a28482bffb59a1c8
reitmas32 <rafa.zamora.rals@gmail.com>
2023-07-08 22:23:18 -0600

    feat: convert to flask_restx

--- src/api/v1/routers/service.py
+++ src/api/v1/routers/service.py
@@ -8,38 +8,58 @@ import config.base as CONFIG
 from tools.functions_authentication import valid_headers
 
 from ..views.service import register_service_route_POST
+from config.api_docs import api
+from flask_restx import Api, Resource, fields, Namespace
 
-views_services = Blueprint("services", __name__)
+service_ns = Namespace('Service', description='Services', path='/api/v1')
 
+register_service_response_201 = api.model('register_service.response.201', {
+    'Data': fields.Raw(required=True, example='[...]'),
+    'Successful': fields.Boolean(required=True, example=True),
+    'Message': fields.String(required=True, example='Successful Response'),
+})
 
-@views_services.route("/api/v1/register_service", methods=["POST"])
-def register_service():
-    """SignUp EndPoint
+register_service_response_401 = api.model('register_service.response.401', {
+  "Data": fields.Raw(required=True, example=[]),
+  "Successful": fields.Boolean(required=True, example=True),
+  "Message": fields.String(required=True, example='Not API_KEY Valid')
+}
+)
 
-    Returns:
-        tuple: tuple of Dict and HTTP Code exmaple
-        {
-            "Data": {
-                .
-                .
-                .
-            },
-            "Message": "....",
-            "Success": true
-        }
-    """
+register_service_request = api.model('ServiceSchema', {
+    'name': fields.String(required=True, example='uniaccounts'),
+    'mother_login': fields.Boolean(required=True, example=True, description='True if you sistem no register personal passwords'),
+    'jwt_exp': fields.Integer(required=True, example=5, description='minutes expired this JWT'),
+})
 
-    api_key=request.headers.get("Api-Key")
+@service_ns.route('/register_service')
+class ServicesResource(Resource):
+    @api.doc(
+    responses={
+        201: 'Success signup, user create',
+        401: 'Not API_KEY Valid',
+        },
+    headers={'Api-Key': 'sEw7CMS03iJRI3EzVm1sYHfeqwQT6uZh'},
+    description='Create a new user on the specified system and service'
+    )
+    @api.marshal_with(register_service_response_201, code=201)
+    @api.marshal_with(register_service_response_401, code=401)
+    @api.expect(register_service_request, validate=False)
+    def post(self):
 
-    if api_key != CONFIG.API_KEY:
-        response = {
-            "Success": False,
-            "Message": "Not API_KEY Valid",
-            "Data": {},
-        }
+        api_key=request.headers.get("Api-Key")
 
-        return response
+        if api_key != CONFIG.API_KEY:
+            response = {
+                "Success": False,
+                "Message": "Not API_KEY Valid",
+                "Data": {},
+            }
 
-    if request.method == "POST":
-        response = register_service_route_POST(parameters_json=request.get_json())
-        return response
+            return response
+
+        if request.method == "POST":
+            response = register_service_route_POST(parameters_json=request.get_json())
+            return response
+
+api.add_namespace(service_ns)
